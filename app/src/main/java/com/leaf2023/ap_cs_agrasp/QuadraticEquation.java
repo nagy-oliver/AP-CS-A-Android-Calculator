b0242d124deb913514db7164473b5e68f4d2f770
nagy-oliver <oliver.nagy510@gmail.com>
2021-11-22 22:45:16 +0100

    Added activities

---
+++ app/src/main/java/com/leaf2023/ap_cs_agrasp/QuadraticEquation.java
@@ -0,0 +1,14 @@
+package com.leaf2023.ap_cs_agrasp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuadraticEquation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quadratic_equation);
+    }
+}
