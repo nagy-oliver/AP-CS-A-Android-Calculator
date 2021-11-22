f7d58ae30f3f372c10a5602e9f07a01c340891c5
Lampask <adamgramblicka@hotmail.com>
2021-11-22 22:48:11 +0100

    Added comments and destinations

--- app/src/main/java/com/leaf2023/ap_cs_agrasp/MainActivity.java
+++ app/src/main/java/com/leaf2023/ap_cs_agrasp/MainActivity.java
@@ -1,12 +1,10 @@
 package com.leaf2023.ap_cs_agrasp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-import java.security.KeyPair;
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,10 +16,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Create map that holds info about all calculator destinations -> where to navigate
         Map<Integer, Intent> options = Collections.unmodifiableMap(new HashMap<Integer, Intent>() {{
-            put(R.id.button, new Intent(getBaseContext(), MainActivity.class));
+            put(R.id.button, new Intent(getBaseContext(), QuadraticEquation.class));
+            put(R.id.button2, new Intent(getBaseContext(), Nutrition.class));
+            put(R.id.button3, new Intent(getBaseContext(), StudyHours.class));
+            put(R.id.button4, new Intent(getBaseContext(), BaseConversion.class));
+            put(R.id.button5, new Intent(getBaseContext(), RNG.class));
         }});
 
+        // Attach layout change to listener
         for (HashMap.Entry<Integer, Intent> a: options.entrySet()) {
             findViewById(a.getKey()).setOnClickListener(view -> { startActivity(a.getValue()); });
         }
