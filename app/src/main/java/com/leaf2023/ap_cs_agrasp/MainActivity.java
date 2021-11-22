fbbc7b40aa46da3229eda98f26ebd98b4521ef03
Lampask <adamgramblicka@hotmail.com>
2021-11-22 22:32:12 +0100

    Add transitions base

--- app/src/main/java/com/leaf2023/ap_cs_agrasp/MainActivity.java
+++ app/src/main/java/com/leaf2023/ap_cs_agrasp/MainActivity.java
@@ -2,7 +2,14 @@ package com.leaf2023.ap_cs_agrasp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import java.security.KeyPair;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +18,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        Map<Integer, Intent> options = Collections.unmodifiableMap(new HashMap<Integer, Intent>() {{
+            put(R.id.button, new Intent(getBaseContext(), MainActivity.class));
+        }});
 
+        for (HashMap.Entry<Integer, Intent> a: options.entrySet()) {
+            findViewById(a.getKey()).setOnClickListener(view -> { startActivity(a.getValue()); });
+        }
     }
 }
