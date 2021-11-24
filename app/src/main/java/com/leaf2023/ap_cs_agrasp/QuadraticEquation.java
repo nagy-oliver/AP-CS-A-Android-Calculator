0939b18a9b08924607c8b5cb40af2d3e1ba43467
Lampask <adamgramblicka@hotmail.com>
2021-11-24 22:54:07 +0100

    Add more universal exception and comments

--- app/src/main/java/com/leaf2023/ap_cs_agrasp/QuadraticEquation.java
+++ app/src/main/java/com/leaf2023/ap_cs_agrasp/QuadraticEquation.java
@@ -18,26 +18,33 @@ public class QuadraticEquation extends AppCompatActivity {
         EditText ctext = findViewById(R.id.editTextNumberDecimal3);
         TextView result = findViewById(R.id.resultStudy);
 
-        class RootsIrrationalException extends Exception {
-            public RootsIrrationalException(String errorMessage) {
+        // An example of an exception
+        class QuadComputationException extends Exception {
+            public QuadComputationException(String errorMessage) {
                 super(errorMessage);
             }
         }
 
+        // Register listener
         findViewById(R.id.buttonSubmitStudy).setOnClickListener(view -> {
             try {
+                // Parse data from string input
                 double a = Double.parseDouble((!atext.getText().toString().equals("") ? atext.getText().toString() : "0"));
                 double b = Double.parseDouble((!btext.getText().toString().equals("") ? btext.getText().toString() : "0"));
                 double c = Double.parseDouble((!ctext.getText().toString().equals("") ? ctext.getText().toString() : "0"));
 
+                // Do calculations
                 double disc = Math.sqrt(Math.pow(b, 2) - 4 * a * c);
                 double positive = (-b + disc) / (2 * a);
                 double negative = (-b - disc) / (2 * a);
-                if (Double.isNaN(positive) && Double.isNaN(negative)) throw new RootsIrrationalException("Both roots are imaginary");
+
+                if (a == 0) throw new QuadComputationException("Parameter `A` cannot be zero");
+                if (Double.isNaN(positive) && Double.isNaN(negative)) throw new QuadComputationException("Both roots are imaginary");
+
                 result.setText(String.format("x = { %f , %f }", positive, negative));
             } catch (NumberFormatException x) {
                 result.setText("Please fill correct values");
-            } catch (RootsIrrationalException x) {
+            } catch (QuadComputationException x) {
                 result.setText(x.getMessage());
             }
         });
