14f73e65c47a65025dc6eedef3055c67488c470c
nagy-oliver <oliver.nagy510@gmail.com>
2021-11-25 00:39:25 +0100

    Added some more comments

--- app/src/main/java/com/leaf2023/ap_cs_agrasp/Nutrition.java
+++ app/src/main/java/com/leaf2023/ap_cs_agrasp/Nutrition.java
@@ -13,17 +13,21 @@ public class Nutrition extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nutrition);
+
         //saving .xml elements as variables
         EditText weightField = findViewById(R.id.editTextNumberDecimalNum);
         RadioButton male = findViewById(R.id.radioButton);
         RadioButton female = findViewById(R.id.radioButton2);
         TextView result = findViewById(R.id.textView3);
+
         //sets male true by default
         male.setChecked(true);
+
         //what happens after clicking Calculate button
         findViewById(R.id.buttonGetCalories).setOnClickListener(view -> {
-            boolean gender = !male.isChecked() && female.isChecked();
+            boolean gender = !male.isChecked() && female.isChecked(); //finding the gender selected
             int weight = Integer.parseInt((!weightField.getText().toString().equals("") ? weightField.getText().toString() : "0"));
+
             //calculates the BMR calories based on a set formula
             result.setText(""+(int)(gender?0.9:1.0) * weight * 24+" Calories/day");
         });
--- app/src/main/java/com/leaf2023/ap_cs_agrasp/RNG.java
+++ app/src/main/java/com/leaf2023/ap_cs_agrasp/RNG.java
@@ -14,13 +14,19 @@ public class RNG extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rng);
 
+        //saving .xml elements as variables
         EditText fromField = findViewById(R.id.editTextNumber);
         EditText toField = findViewById(R.id.editTextNumber2);
         TextView result = findViewById(R.id.resultNum);
 
+        //what happens after clicking Generate button
         findViewById(R.id.buttonGenerate).setOnClickListener(view -> {
+
+            //replacing empty fields by 0
             int from = Integer.parseInt((!fromField.getText().toString().equals("") ? fromField.getText().toString() : "0"));
             int to = Integer.parseInt((!toField.getText().toString().equals("") ? toField.getText().toString() : "0"));
+
+            //generating random number in the range given
             result.setText(""+(int)(Math.random()*(to-from+1)+from));
         });
     }
