1d1a7df4a51467111a055161223b69684fca4c20
nagy-oliver <oliver.nagy510@gmail.com>
2021-11-24 22:56:28 +0100

    Nutrition calculator started with the code part

--- app/src/main/java/com/leaf2023/ap_cs_agrasp/Nutrition.java
+++ app/src/main/java/com/leaf2023/ap_cs_agrasp/Nutrition.java
@@ -3,6 +3,9 @@ package com.leaf2023.ap_cs_agrasp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class Nutrition extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Nutrition extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nutrition);
+
+        EditText weight = findViewById(R.id.editTextNumberDecimalTerm);
+        RadioButton male = findViewById(R.id.radioButton);
+        RadioButton female = findViewById(R.id.radioButton2);
+        TextView result = findViewById(R.id.textView3);
+
+        findViewById(R.id.buttonSubmitStudy).setOnClickListener(view -> {
+
+        });
+
     }
 }
