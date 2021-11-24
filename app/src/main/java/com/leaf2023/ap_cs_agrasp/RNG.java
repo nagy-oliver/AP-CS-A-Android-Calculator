a4f2f513a649c332ee93f25203a01e594bd5feca
nagy-oliver <oliver.nagy510@gmail.com>
2021-11-25 00:28:27 +0100

    Finished RNG

--- app/src/main/java/com/leaf2023/ap_cs_agrasp/RNG.java
+++ app/src/main/java/com/leaf2023/ap_cs_agrasp/RNG.java
@@ -3,6 +3,9 @@ package com.leaf2023.ap_cs_agrasp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import java.lang.Math;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RNG extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class RNG extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rng);
+
+        EditText fromField = findViewById(R.id.editTextNumber);
+        EditText toField = findViewById(R.id.editTextNumber2);
+        TextView result = findViewById(R.id.resultNum);
+
+        findViewById(R.id.buttonGenerate).setOnClickListener(view -> {
+            int from = Integer.parseInt((!fromField.getText().toString().equals("") ? fromField.getText().toString() : "0"));
+            int to = Integer.parseInt((!toField.getText().toString().equals("") ? toField.getText().toString() : "0"));
+            result.setText(""+(int)(Math.random()*(to-from+1)+from));
+        });
     }
 }
